1f8a81f4cbf0dcca5837d40c55eb1b203e8fd7a9
Oladipopreserve-19 <98063668+Oladipopreserve-19@users.noreply.github.com>
2022-12-02 12:13:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function change() {
+// select element from DOM using *const*
+const sample = document.getElementById("box");
+sample.style.background = 'blue'
+}
+
+function bod() {
+// select element from DOM using *const*
+const caps = document.getElementById("body");
+caps.style.background = 'black'
+}
+
